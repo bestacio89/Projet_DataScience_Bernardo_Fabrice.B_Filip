f925e8ee2e0d29453c3ed1e224147005bac56c1f
Bernardo Estacio Abreu <bernardo.estacio89@gmail.com>
2024-11-18 14:08:25 +0100

    [Fix] Streamlit Base

--- streamlit/app/app.py
+++ streamlit/app/app.py
@@ -1,41 +1,25 @@
 import streamlit as st
-from streamlit.modelised_classes.diabetes_prediction_ml_module import DiabetesPredictionML
-from streamlit.modelised_classes.diabetes_prediction_module import DiabetesPredictionApp
 
 st.title("Application de Data Science")
 
-# Sidebar Menu
-page = st.sidebar.selectbox("Choisissez une page:", ["Accueil", "Régression", "Classification (Deep Learning)", "Classification (ML)"])
+# Menu
+page = st.sidebar.selectbox("Choisissez une page:", ["Accueil", "Régression", "Classification", "Ongles"])
 
 if page == "Accueil":
-    st.write("Bienvenue sur l'application de Data Science!")
-    st.markdown("""
-    Naviguez à travers les pages pour explorer :
-    - **Régression** : Cas d'utilisation avec des données continues.
-    - **Classification (Deep Learning)** : Modèles de réseaux de neurones pour la classification.
-    - **Classification (ML)** : Modèles de Machine Learning pour la classification.
-    """)
+  st.write("Bienvenue sur l'application de Data Science!")
+  # ... (Ajouter du contenu à la page d'accueil)
 
-elif page == "Classification (Deep Learning)":
-    st.write("## Classification du diabète avec Deep Learning")
-    filepath = st.text_input("Entrez le chemin vers le dataset :", value="data/diabete.csv")
+elif page == "Régression":
+  # Exécuter le script de régression
+  st.write("## Régression sur la qualité du vin")
+  # ... (Code pour la régression, similaire à l'exemple précédent)
 
-    if st.button("Exécuter"):
-        if filepath:
-            diabetes_app = DiabetesPredictionApp(filepath)
-            diabetes_app.execute_workflow()
-            st.success("Workflow terminé avec succès ! Modèle sauvegardé.")
-        else:
-            st.error("Veuillez entrer un chemin valide pour le dataset.")
+elif page == "Classification":
+  # Exécuter le script de classification
+  st.write("## Classification du diabète")
+  # ... (Code pour la classification, similaire à l'exemple précédent)
 
-elif page == "Classification (ML)":
-    st.write("## Classification du diabète avec Machine Learning")
-    filepath = st.text_input("Entrez le chemin vers le dataset :", value="data/diabete.csv")
-
-    if st.button("Exécuter"):
-        if filepath:
-            diabetes_ml_app = DiabetesPredictionML(filepath)
-            diabetes_ml_app.execute_workflow()
-            st.success("Workflow terminé avec succès ! Modèles sauvegardés.")
-        else:
-            st.error("Veuillez entrer un chemin valide pour le dataset.")
+elif page == "Ongles":
+  # Exécuter le script d'analyse d'ongles
+  st.write("## Analyse d'images d'ongles")
+  # ... (Code pour l'analyse d'ongles, similaire à l'exemple précédent)
